2176ddb899459dcb5d74f179b8e6d8f5d83bf5bf
GrearNose <macjecy@Gmail.com>
2018-08-12 22:23:02 +0800

    correct a bug that it will end up in a deap-loop when the query is less than acc[0].

--- jobHunting/NetEase_2018_08_11/apple_stack.py
+++ jobHunting/NetEase_2018_08_11/apple_stack.py
@@ -1,3 +1,15 @@
+# Problem Description
+# There are n stack apples, and the i-th has the number of Ai.
+# Given a query q, figure out which stack the q-th apple belongs to.
+# input:
+# the 1st row, an integer n, the number of apple stacks;
+# the 2nd row, n integers, the amount of each stacks;
+# the 3rd row, an integer m, the number of queries;
+# the 4th row, m integers, the queries.
+# outputs:
+# m rows, each for the answer to the corresponding queires.
+
+
 n = int(input())
 A = [int(x) for x in input().split()]
 m = int(input())
@@ -15,19 +27,22 @@ for x in A:
 
 for a in q:
     if a < 0 or a > acc[-1]:
+        print(-1)
+        continue
+    if a < acc[0]:
         print(1)
         continue
 
     l,h = 0,n-1
     while l <= h:
-        mid = (l+h)//2
+        mid = (l+h) >> 1
 
         if acc[mid-1] < a and a <= acc[mid]:
             print(mid+1)
             break
 
-        elif mid > 0 and a < acc[mid-1]:
+        elif a < acc[mid-1]:
             h = mid-1
 
-        elif acc[mid] < a and mid < n:
-            l = mid +1
+        elif acc[mid] < a:
+            l = mid+1
